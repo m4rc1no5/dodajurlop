b99afe24961def715458add4c44611e7bfe0fa10
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-09-06 14:05:40 +0200

    Install FOSUserBundle

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 dodajurlop
 ==========
 
+[![Build Status](https://travis-ci.org/m4rc1no5/dodajurlop.svg?branch=master)](https://travis-ci.org/m4rc1no5/dodajurlop)
+
 A Symfony project created on September 5, 2015, 10:11 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcinos
+ * Date: 06.09.15
+ * Time: 14:20
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @package AppBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=256)
+     *
+     */
+    protected $komentarz;
+
+}
