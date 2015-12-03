110421934fd153a9ad95c2ec6a3e4fb2be8da80c
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-12-03 18:52:06 +0100

    Test UrlopController - indexAction

--- src/AppBundle/Tests/Controller/UrlopControllerTest.php
+++ src/AppBundle/Tests/Controller/UrlopControllerTest.php
@@ -9,12 +9,34 @@
 namespace AppBundle\Tests\Controller;
 
 
+use AppBundle\CommandBus\Urlop\DodajUrlopCommand;
+use AppBundle\Controller\UrlopController;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Mockery as M;
 
 class UrlopControllerTest extends TestCase
 {
-    public function test()
-    {
 
+    /** @var M\Mock */
+    private $dodajUrlopCommand;
+
+    public function setUp()
+    {
+        $this->dodajUrlopCommand = M::mock(DodajUrlopCommand::class);
     }
+
+    public function testIndexAction()
+    {
+        $controller = $this->getUrlopController();
+
+        $ar_dane = $controller->indexAction();
+
+        $this->assertArrayHasKey('urlopy', $ar_dane);
+    }
+
+    private function getUrlopController()
+    {
+        return new UrlopController($this->dodajUrlopCommand);
+    }
+
 }
