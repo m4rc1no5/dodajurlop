7203195834cdf12e9204853d1b2171d9c0ba3492
mistrzJoda <menshikov.ivn@gmail.com>
2015-12-04 17:41:44 +0100

    Jedny z ostatnich testów:)

--- src/AppBundle/Tests/Controller/UrlopControllerTest.php
+++ src/AppBundle/Tests/Controller/UrlopControllerTest.php
@@ -11,8 +11,14 @@ namespace AppBundle\Tests\Controller;
 
 use AppBundle\CommandBus\Urlop\DodajUrlopCommand;
 use AppBundle\Controller\UrlopController;
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 use Mockery as M;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UrlopControllerTest extends TestCase
 {
@@ -32,6 +38,30 @@ class UrlopControllerTest extends TestCase
         $ar_dane = $controller->indexAction();
 
         $this->assertArrayHasKey('urlopy', $ar_dane);
+    }
+
+    public function testDodaj()
+    {
+        $controller = $this->getUrlopController();
+
+        $container = M::mock(ContainerInterface::class);
+        $user_zalogowany = M::mock(User::class);
+        $token = M::mock();
+        $token_storage = M::mock(TokenStorageInterface::class);
+        $token->shouldReceive('getUser')->once()->andReturn($user_zalogowany);
+        $token_storage->shouldReceive('getToken')->once()->andReturn($token);
+        $container->shouldReceive('has')->with('security.token_storage')->once()->andReturn(true);
+        $container->shouldReceive('get')->with('security.token_storage')->once()->andReturn($token_storage);
+        $form_factory = M::Mock(FormFactoryInterface::class);
+        $form = M::Mock(Form::class);
+        $form->shouldReceive('handleRequest')->once()->andReturn(true);
+        $form->shouldReceive('createView')->once()->andReturn('stworzyła się jakaś forma');
+        $form_factory->shouldReceive('create')->once()->andReturn($form);
+        $container->shouldReceive('get')->with('form.factory')->once()->andReturn($form_factory);
+        $controller->setContainer($container);
+
+        $ar_dane = $controller->dodajAction(new Request());
+        $this->assertEquals('stworzyła się jakaś forma', $ar_dane['form']);
     }
 
     private function getUrlopController()
--- src/AppBundle/Tests/Form/Type/OrganizacjaSimpleTypeTest.php
+++ src/AppBundle/Tests/Form/Type/OrganizacjaSimpleTypeTest.php
@@ -9,10 +9,12 @@
 namespace AppBundle\Tests\Form\Type;
 
 
+use AppBundle\Entity\User;
 use AppBundle\Form\Type\OrganizacjaSimpleType;
 use AppBundle\Repository\Doctrine\OrganizacjaRepository;
 use Symfony\Component\Form\Test\TypeTestCase;
 use Mockery as M;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
@@ -51,10 +53,19 @@ class OrganizacjaSimpleTypeTest extends TypeTestCase
         $this->assertEquals('AppBundle\Entity\Organizacja', $o::DATA_CLASS);
     }
 
+    public function testConfigureOptions()
+    {
+        $o = $this->getOrganizacjaSimpleType();
+        $resolver = M::mock(OptionsResolver::class);
+        $resolver->shouldReceive('setDefaults')->with(['class' => 'AppBundle\Entity\Organizacja', 'choices' => 'choices']);
+        $o->configureOptions($resolver);
+    }
+
     private function getOrganizacjaSimpleType()
     {
-        $this->token->shouldReceive('getUser')->once();
+        $this->token->shouldReceive('getUser')->once()->andReturn(new User());
         $this->tokenStorage->shouldReceive('getToken')->once()->andReturn($this->token);
+        $this->organizacjaRepository->shouldReceive('findAllByUser')->with(User::class)->andReturn('choices');
         return new OrganizacjaSimpleType($this->organizacjaRepository, $this->tokenStorage);
     }
 }
