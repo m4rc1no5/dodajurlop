0b496744ee3756cfa2f0493f69388a81372dba51
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-12-06 07:25:00 +0100

    Test dodania urlopu do repo

--- src/AppBundle/Tests/Repository/Doctrine/UrlopRepositoryTest.php
+++ src/AppBundle/Tests/Repository/Doctrine/UrlopRepositoryTest.php
@@ -9,6 +9,11 @@
 namespace AppBundle\Tests\Repository\Doctrine;
 
 
+use AppBundle\Entity\Organizacja;
+use AppBundle\Entity\Pracownik;
+use AppBundle\Entity\Urlop;
+use AppBundle\Entity\UrlopRodzaj;
+use AppBundle\Entity\User;
 use AppBundle\Repository\Doctrine\UrlopRepository;
 use AppBundle\Tests\AppTestCase;
 use Doctrine\ORM\EntityManager;
@@ -17,12 +22,38 @@ use Mockery as M;
 
 class UrlopRepositoryTest extends AppTestCase
 {
+
+    /** @var string */
     private $entity_class_name = 'AppBundle\Entity\Urlop';
 
-    public function testAdd()
+    /** @var M\Mock */
+    private $em;
+
+    protected function setUp()
+    {
+        $this->em = M::mock(EntityManager::class);
+    }
+
+    /**
+     * Test dodania urlopu do repo
+     */
+    public function testAddUrlop()
     {
         $urlopRepository = $this->getUrlopRepository();
 
+        $urlop = new Urlop(
+            M::mock(User::class),
+            M::mock(UrlopRodzaj::class),
+            M::mock(Pracownik::class),
+            M::mock(Organizacja::class),
+            new \DateTime(),
+            new \DateTime(),
+            '26',
+            '2015'
+        );
+
+        $this->em->shouldReceive('persist')->once();
+        $urlopRepository->add($urlop);
     }
 
     public function testEntityClassName()
@@ -34,8 +65,7 @@ class UrlopRepositoryTest extends AppTestCase
 
     private function getUrlopRepository()
     {
-        $em = M::mock(EntityManager::class);
-        $em->shouldReceive('getClassMetadata')->with($this->entity_class_name)->andReturn(M::mock(ClassMetadata::class));
-        return new UrlopRepository($em);
+        $this->em->shouldReceive('getClassMetadata')->with($this->entity_class_name)->andReturn(M::mock(ClassMetadata::class));
+        return new UrlopRepository($this->em);
     }
 }
